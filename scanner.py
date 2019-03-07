35b289b998116a7ecd51f1587aaeafed571b565a
lazlo54 <32623331+lazlo54@users.noreply.github.com>
2019-03-07 17:27:41 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,34 +1,13 @@
-"""
-Sample script to test ad-hoc scanning by table drive.
-This accepts a number with optional decimal part [0-9]+(\.[0-9]+)?
-
-NOTE: suitable for optional matches
-"""
-
 def getchar(text,pos):
-	""" returns char category at position `pos` of `text`,
-	or None if out of bounds """
-	
+    
 	if pos<0 or pos>=len(text): return None
 	
 	c = text[pos]
 	
-	# **Σημείο #3**: Προαιρετικά, προσθέστε τις δικές σας ομαδοποιήσεις
+	return c
 	
-	if c>='0' and c<='9': return 'DIGIT'	# 0..9 grouped together
-	
-	if c=='.': return 'DOT'	# dot as a category by itself
-	
-	return c	# anything else
-	
-
-
 def scan(text,transitions,accepts):
-	""" scans `text` while transitions exist in
-	'transitions'. After that, if in a state belonging to
-	`accepts`, it returns the corresponding token, else ERROR_TOKEN.
-	"""
-	
+
 	# initial state
 	pos = 0
 	state = 's0'
@@ -59,21 +38,29 @@ def scan(text,transitions,accepts):
 			return 'ERROR_TOKEN',pos
 			
 	
-# **Σημείο #1**: Αντικαταστήστε με το δικό σας λεξικό μεταβάσεων
-transitions = { 's0': { 'DIGIT':'s1' },
-       			's1': { 'DIGIT':'s1','DOT':'s2' },
-       			's2': { 'DIGIT':'s3' },
-       			's3': { 'DIGIT':'s3' }       
-     		  } 
+transitions = {'s0' :{'0': 's1','1': 's1','2': 's1','3': 's2'},
+    's1': {'0': 's3','1': 's3','2': 's3','3': 's3','4': 's3','5': 's3','6': 's3','7': 's3','8': 's3','9': 's3'},
+    's2': {'0': 's4','1': 's4','2': 's4','3': 's4','4': 's4','5': 's4'},
+    's3': {'0': 's5'},
+    's4': {'0': 's5'},
+    's5': {'0': 's6','1': 's6','2': 's6','3': 's6','4': 's6','5': 's6','6': 's6','7': 's6','8': 's6','9': 's6'},
+    's6': {'0': 's7','1': 's7','2': 's7','3': 's7','4': 's7','5': 's7','6': 's7','7': 's7','8': 's7','9': 's7'},
+    's7': {'K': 's8','M': 's10','G': 's12'},
+    's8': {'T': 's9'},
+    's10': {'P': 's11'},
+    's11': {'S': 's9'},
+    's12': {'0': 's13','1': 's13','2': 's13','3': 's13','4': 's13','5': 's13','6': 's13','7': 's13','8': 's13','9': 's13'},
+    's13': {'0': 's14','1': 's14','2': 's14','3': 's14','4': 's14','5': 's14','6': 's14','7': 's14','8': 's14','9': 's14'},
+    's14': {'K': 's8','M': 's10'}
+}
 
-# **Σημείο #2**: Αντικαταστήστε με το δικό σας λεξικό καταστάσεων αποδοχής
-accepts = { 's1':'INT_TOKEN',
-       		's3':'FLOAT_TOKEN'	
+
+accepts = { 's9':'WIND_TOKEN'
      	  }
 
 
 # get a string from input
-text = input('give some input>')
+text = input('give some input >')
 
 # scan text until no more input
 while text:		# i.e. len(text)>0
@@ -85,4 +72,3 @@ while text:		# i.e. len(text)>0
 	print("token:",token,"text:",text[:pos])
 	# new text for next scan
 	text = text[pos:]
-	
